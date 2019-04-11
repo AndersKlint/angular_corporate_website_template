ecc0e51aa770fa15920b579369d4c4ecd34899c2
Anders Klint <anders.klint519@gmail.com>
2019-04-12 00:12:58 +0200

    home kinda done

--- src/app/material-imports.ts
+++ src/app/material-imports.ts
@@ -2,20 +2,23 @@ import {NgModule} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule, MatIconModule} from '@angular/material';
 import { MatCarouselModule } from '@ngmodule/material-carousel';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   imports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatCarouselModule
+    MatCarouselModule,
+    MatExpansionModule,
   ],
   exports:
   [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatCarouselModule
+    MatCarouselModule,
+    MatExpansionModule,
   ]
 })
 export class MaterialImports {}
