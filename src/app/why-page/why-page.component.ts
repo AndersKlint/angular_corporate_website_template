6a16df471a21e50f1d61198f60e3e2ba5a643ca4
Anders Klint <anders.klint519@gmail.com>
2019-04-14 17:15:25 +0200

    why page images and stuff. Needs refactor: move css classes of images to global styles.css

--- src/app/why-page/why-page.component.ts
+++ src/app/why-page/why-page.component.ts
@@ -3,9 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-why-page',
   templateUrl: './why-page.component.html',
-  styleUrls: ['./why-page.component.sass']
+  styleUrls: ['./why-page.component.scss']
 })
 export class WhyPageComponent implements OnInit {
+
+  slides: string[] = [
+    'assets/alps_lake.jpg',
+    'assets/beach_bridge.jpg',
+    'assets/dragon_boat.jpg',
+    'assets/paper_art.jpg',
+    'assets/snow_structure.jpg',
+  ]
 
   constructor() { }
 
---
+++ update_git_pages.sh
@@ -0,0 +1,3 @@
+ng build --prod --base-href "https://andersklint.github.io/angular_personal_website_template/"
+
+npx angular-cli-ghpages --dir=dist/personal-website-template
